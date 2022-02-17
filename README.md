f577d7aebf1378d2c63f93e435aaf980978af5d9
emanuele <tommaneo@gmail.com>
2022-02-17 18:15:53 +0100

    Lezione 17 Febbraio

---
+++ README.md
@@ -0,0 +1,1 @@
+# Corso-Docenti-Labriola
---
+++ programma1.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+from matplotlib.pyplot import show
+hideturtle()
+color("red")
+forward(100)
+left(20)
+showturtle()
+forward(100)
+
+print('%10.2f'% (xcor()) )
+
+done()
